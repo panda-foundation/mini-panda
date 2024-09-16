ae8d83c3aed7ca0c5c1894512acfbad5aa4ba23c
Huacai Sang <sanghuacai@gmail.com>
2024-09-17 00:09:41 +0200

    init project

---
+++ MicroPandaTest/Token/TokenTest.cs
@@ -0,0 +1,14 @@
+﻿namespace MicroPandaTest.Token;
+
+using MicroPanda.Token;
+
+[TestClass]
+public class TokenTest
+{
+    [TestMethod]
+    public void TestTokenUtil()
+    {
+        Assert.IsTrue(true);
+        Assert.AreEqual((int)Token.ILLEGAL, 0);
+    }
+}
